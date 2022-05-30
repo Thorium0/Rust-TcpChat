b4febbcb4aea4e7493c936117a1e72f0d694916f
Troels Wittrup-Jensen <troelswittrupjensen@gmail.com>
2022-05-30 14:25:16 +0200

    Added info messages to chat-window

--- dist/connector.js
+++ dist/connector.js
@@ -1,5 +1,28 @@
 const invoke = window.__TAURI__.invoke
 const emit = window.__TAURI__.event.emit
+const listen = window.__TAURI__.event.listen
+
+
+
+$(document).ready(function() {
+    var input = document.getElementById("msg");
+    var ipaddr = document.getElementById("ipaddr");
+
+    input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          $("#send-btn").click();
+        }
+    });
+
+    ipaddr.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          $("#connect-btn").click();
+        }
+    });
+
+});
 
 
 $("#connect-btn").click(function() {
@@ -14,6 +37,28 @@ $("#send-btn").click(function() {
     var msg_elem = $("#msg");
     var msg = msg_elem.val();
     msg_elem.val("");
-
-    emit("send_msg", { msg: msg });
+    if (msg.trim() != "") {
+        emit("send_msg", { msg: msg });
+    }
 });
+
+
+listen('add_to_chatbox', event => {
+    var chatbox_elem = $("#chatbox");
+    var payload = event.payload;
+    var user = payload["user"];
+    var message = "["+user+"] "+payload["message"]+"\n\n";
+    chatbox_elem.append(message);
+    chatbox_elem.scrollTop(chatbox_elem[0].scrollHeight);
+});
+
+
+listen('add_info_to_chatbox', event => {
+    var chatbox_elem = $("#chatbox");
+    var payload = event.payload;
+    var kind = payload["kind"];
+    var message = "{"+kind+"} "+payload["message"]+"\n\n";
+    chatbox_elem.append(message);
+    chatbox_elem.scrollTop(chatbox_elem[0].scrollHeight);
+});
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,28 +4,33 @@
 )]
 
 use serde::{Serialize, Deserialize};
-use tauri::api::Error;
-use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, Manager, App};
-use std::io::{self, ErrorKind, Read, Write};
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, Manager, Window};
+use std::io::{ErrorKind, Read, Write};
 use std::net::{TcpStream};
 use std::sync::mpsc::{Receiver, Sender, TryRecvError};
 use tokio::task;
-use std::sync::{Mutex, Arc};
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
-use tauri::async_runtime::{self};
 use lazy_static::lazy_static;
+use json;
+use serde_json;
 
-const MSG_SIZE: usize = 32;
+const MSG_SIZE: usize = 512;
 const PORT: &str = "2478";
+const CONNECTION_TRIES: u8 = 5;
 
 lazy_static! {
   static ref CHANNEL: Mutex<Option<Sender<String>>> = Mutex::new(None);
+  static ref MSG_TO_SEND: Mutex<String> = Mutex::new(String::new());
+  static ref INFO_MSG: Mutex<String> = Mutex::new(String::new());
+  static ref USER: Mutex<String> = Mutex::new(String::new());
+  static ref IS_RUNNING: Mutex<bool> = Mutex::new(false);
 }
 
 
 
-fn start() -> Menu {
+fn get_menu() -> Menu {
   let new = CustomMenuItem::new("new".to_string(), "New");
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
   let about = CustomMenuItem::new("about".to_string(), "About");
@@ -42,91 +47,177 @@ fn start() -> Menu {
   
 }
 
+async fn connect(name: String, mut ipaddr: String) {
 
+  ipaddr.push_str(":");
+  ipaddr.push_str(PORT);
 
-  async fn connect(name: String, mut ipaddr: String) {
+  display_info_msg("Connecting to server...", "Info");
+  let mut stream_type: Option<TcpStream> = None;
+  for _ in 0..CONNECTION_TRIES {
+    let stream_result = TcpStream::connect(&ipaddr);
+    if stream_result.is_ok() {
+      stream_type = Some(stream_result.unwrap());
+      break;
+    }
 
-    ipaddr.push_str(":");
-    ipaddr.push_str(PORT);
+    thread::sleep(Duration::from_millis(1000));
+  }
+
+  let mut stream;
+  if stream_type.is_some() {
+    display_info_msg("Connected to server.", "Success");
+    stream = stream_type.unwrap();
+  } else {
+    display_info_msg("Could not connect to server.", "Error");
+    return;
+  }
+  stream.set_nonblocking(true).expect("Non-blocking failed");
+
+  let tx: Sender<String>;
+  let rx: Receiver<String>;
+
+  (tx, rx) = std::sync::mpsc::channel::<String>();
+
+  *CHANNEL.lock().unwrap() = Some(tx);
+  *USER.lock().unwrap() = name;
+  *IS_RUNNING.lock().unwrap() = true;
     
-    let mut stream = TcpStream::connect(ipaddr).expect("Connection Error");
-    stream.set_nonblocking(true).expect("Non-blocking failed");
 
-    let tx: Sender<String>;
-    let mut rx: Receiver<String>;
+  let conn = thread::spawn(move || loop {
 
-    (tx, rx) = std::sync::mpsc::channel::<String>();
-
-    *CHANNEL.lock().unwrap() = Some(tx);
-    
-
-    let conn = thread::spawn(move || loop {
-
-      let mut buff = vec![0; MSG_SIZE];
-      match stream.read_exact(&mut buff) {
-        Ok(_) => {
-          let msg = String::from_utf8(buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>());
-          println!("Message resv {:?}", msg);
-        },
-        Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-        Err(_) => {
-          println!("Connection to server broke");
-          break;
-        }
+  let mut buff = vec![0; MSG_SIZE];
+  match stream.read_exact(&mut buff) {
+    Ok(_) => {
+      let msg = String::from_utf8(buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>());
+      if msg.is_ok() {
+        println!("Message resv {:?}", msg);
+        *MSG_TO_SEND.lock().unwrap() = msg.unwrap();
+      } else {
+        println!("Error parsing message");
       }
+    },
+    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+    Err(_) => {
+      display_info_msg("Connection to server broke.", "Alert");
+      break;
+    }
+  }
       
-      match rx.try_recv() {
-        Ok(msg) => {
-          let mut buff = msg.clone().into_bytes();
-          buff.resize(MSG_SIZE, 0);
-          stream.write_all(&buff).expect("Writing to socket failed");
-          println!("Message sent {:?}", msg);
-        },
-        Err(TryRecvError::Empty) => (),
-        Err(TryRecvError::Disconnected) => break
-      }
+  match rx.try_recv() {
+    Ok(msg) => {
+      let mut buff = msg.clone().into_bytes();
+      buff.resize(MSG_SIZE, 0);
+      stream.write_all(&buff).expect("Writing to socket failed");
+      println!("Message sent {:?}", msg);
+    },
+    Err(TryRecvError::Empty) => (),
+    Err(TryRecvError::Disconnected) => break
+  }
   
-      thread::sleep(Duration::from_millis(100));
-    });
+  thread::sleep(Duration::from_millis(100));
+});
    
-
     conn.join().unwrap();
+    *IS_RUNNING.lock().unwrap() = false;
   
-
   }
 
 
+fn display_info_msg(msg: &str, kind: &str) {
+  let mut mut_msg = String::from("{\"msg\": \"");
+  mut_msg.push_str(&msg);
+  mut_msg.push_str("\"}");
+  let mut msg_json = json::parse(&mut_msg).unwrap();
+  msg_json.insert("kind", kind).expect("Could not insert kind on info-message");
+  let final_msg = msg_json.to_string();
+
+  println!("{}", final_msg);
+  *INFO_MSG.lock().unwrap() = final_msg;
+}
+
+
+async fn send_msg(msg: String)  {
+
+  let lock = CHANNEL.lock().unwrap();
+  let tx = lock.as_ref().clone().unwrap();
+    
+  let mut msg_json = json::parse(&msg.trim().to_string()).unwrap();
+  msg_json.insert("user", USER.lock().unwrap().to_string()).expect("Json insert failed");
+  let msg = msg_json.to_string();
+  tx.send(msg).expect("Failed to send message");
+    
+  drop(lock);
+    
+}
   
+#[derive(Serialize, Deserialize)]
+struct Connector {
+    name: String,
+    ipaddr: String,
+}
+
+#[derive(Clone, serde::Serialize)]
+struct Message {
+  message: String,
+  user: String
+}
+
+#[derive(Clone, serde::Serialize)]
+struct Info {
+  message: String,
+  kind: String
+}
 
 
-  async fn send_msg(msg: String)  {
+#[tauri::command]
+fn add_to_chatbox(window: Window) {
+  thread::spawn(move || {
+    let mut raw_str_msg;
+    let mut raw_str_info;
+    loop {
+      raw_str_msg = MSG_TO_SEND.lock().unwrap().to_string();
+      raw_str_info = INFO_MSG.lock().unwrap().to_string();
+      if raw_str_msg != "" {
+        let result = json::parse(&MSG_TO_SEND.lock().unwrap().to_string());
+        let msg_json = if result.is_ok() {
+        result.unwrap()
+      } else {
+        continue
+      };
+      let msg = msg_json["msg"].to_string();
+      let user = msg_json["user"].to_string();
 
-    let lock = CHANNEL.lock().unwrap();
-    let tx = lock.as_ref().clone().unwrap();
-    
-    let msg = msg.trim().to_string();
-    tx.send(msg).expect("Failed to send message");
-    
-    drop(lock);
-    
-  }
+      window.emit("add_to_chatbox", Message { message: msg, user }).unwrap();
+      *MSG_TO_SEND.lock().unwrap() = String::new();
 
-  
-  #[derive(Serialize, Deserialize)]
-  struct Connector {
-      name: String,
-      ipaddr: String,
-  }
- 
+
+      } else if raw_str_info != "" {
+        let result = json::parse(&INFO_MSG.lock().unwrap().to_string());
+        let msg_json = if result.is_ok() {
+        result.unwrap()
+      } else {
+        continue
+      };
+      let msg = msg_json["msg"].to_string();
+      let kind = msg_json["kind"].to_string();
+
+      window.emit("add_info_to_chatbox", Info { message: msg, kind }).unwrap();
+      *INFO_MSG.lock().unwrap() = String::new();
+      }
+      thread::sleep(Duration::from_millis(100));
+    }
+  });
+}
 
 fn main() {
-  let menu = start();
-
-  
-  
+  let menu = get_menu();
 
   tauri::Builder::default()
     .setup(|app| {
+
+      let main_window = app.get_window("main").unwrap();
+      add_to_chatbox(main_window);
 
       app.listen_global("connect", |event| {
           
@@ -134,8 +225,13 @@ fn main() {
         let name: String = payload.name;
         let ip: String = payload.ipaddr;
 
-        task::spawn(connect(name, ip));
-        
+        if *IS_RUNNING.lock().unwrap() {
+          display_info_msg("Already connected to server.", "Alert");
+        } else if name == "" {
+          display_info_msg("Name-field is empty.", "Alert");
+        } else {
+          task::spawn(connect(name, ip));
+        }
 
 
       });
@@ -143,17 +239,19 @@ fn main() {
 
       app.listen_global("send_msg", |event| {
 
-        let msg = "dav".to_string();//event.payload().unwrap().to_string();
-
-        task::spawn(send_msg(msg));
+        if *IS_RUNNING.lock().unwrap() {
+          let msg = event.payload().unwrap().to_string();
+          
+          task::spawn(send_msg(msg));
+        } else {
+          display_info_msg("Not connected to server.", "Alert");
+        }
         
-        
-  
-      
       });
 
-      
+
       Ok(())
+      
     })
     .menu(menu)
     .on_menu_event(|event| {
@@ -164,7 +262,7 @@ fn main() {
         _ => {}
       }
     })
-    .invoke_handler(tauri::generate_handler![])
+    .invoke_handler(tauri::generate_handler![add_to_chatbox])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
